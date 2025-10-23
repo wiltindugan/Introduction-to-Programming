e64a68902f7c1530fafd23b6b3b3ff817bbc5744
wiltindugan <wiltindugan0523@icloud.com>
2025-10-23 13:37:48 +0400

    exercise 3 finished

---
+++ exercise 3.py
@@ -0,0 +1,5 @@
+name=input("Enter your name: ")
+hometown=input("Enter your hometown: ")
+age=input("Enter your age: ")
+person={"Name":name,"Hometown":hometown,"Age":age}
+print(person["Name"],person["Hometown"],person["Age"],sep="\n")
