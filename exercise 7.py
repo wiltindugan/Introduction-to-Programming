be7045227269bd214e3444eb3ba89165c84c14e5
wiltindugan <wiltindugan0523@icloud.com>
2025-10-23 17:01:13 +0400

    exercise 7 finished

---
+++ exercise 7.py
@@ -0,0 +1,14 @@
+for x in range(0,51,1):
+    print(x)
+print()
+for x in range(50,-1,-1):
+    print(x)    
+print()
+for x in range(30,51,1):
+    print(x)
+print()
+for x in range(50,9,-2):
+    print(x)
+print()
+for x in range(100,201,5):
+    print(x)
